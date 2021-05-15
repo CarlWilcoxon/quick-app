95ee456611848befcb5943083b8b29ce33a4ea5a
Carl Wilcoxon <62306383+CarlWilcoxon@users.noreply.github.com>
2021-05-15 09:00:40 -0500

    refactoring

---
+++ src/components/App/App.jsx
@@ -0,0 +1,39 @@
+import './App.css';
+import { useState } from 'react';
+import resultItem from '../ResultItem/ResultItem';
+
+function App(props) {
+  const [operand1, setOperand1] = useState(0);
+  const [operand2, setOperand2] = useState(0);
+  const [result, setResult] = useState(null);
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    setResult(operand1 + operand2);
+}
+
+  return (
+    <div className="App">
+      <form onSubmit={handleSubmit}>
+        <input
+          className="operand"
+          value="operand1" 
+          onChange={e => setOperand1(e.target.value)}
+          />
+
+        <input
+          className="operand"
+          value="operand2"
+          onChange={e => setOperand2(e.target.value)}
+          />
+        <input type="submit" value="=" />
+      </form>
+
+    {if (result != null)}
+
+    </div>
+  );
+}
+
+export default App;
+
