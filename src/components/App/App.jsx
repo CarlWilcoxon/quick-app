f8b306d5136e477a39ac2c192973b01516e47bae
Carl Wilcoxon <62306383+CarlWilcoxon@users.noreply.github.com>
2021-05-20 22:16:02 -0500

    progress converting project to server based setup

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,36 @@
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import ResultList from '../ResultList/ResultList';
 
 function App(props) {
   const [operand1, setOperand1] = useState(0);
   const [operand2, setOperand2] = useState(0);
   const [resultArray, setResultArray] = useState([]);
+  const [history, setHistory] = useState([]);
 
   const handleSubmit = (evt) => {
     evt.preventDefault();
-    setResultArray(resultArray => [...resultArray, Number(operand1) + Number(operand2)]);
+    const newResult = Number(operand1) + Number(operand2);
+    setResultArray(resultArray => [...resultArray, newResult]);
+    axios.post('http://localhost:5000/add-history', newResult)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch(error => console.error("Error adding new history."));
+  }
+
+  // const changeOperator
+  useEffect(() => {
+    getHistory();
+  }, [])
+
+  const getHistory = () => {
+    axios.get('http://localhost:5000/get-history')
+      .then((res) => {
+        const pastResults = res.data.pastResults;
+        setHistory(pastResults);
+      })
+      .catch(error => console.error("Error getting history."));
   }
 
   return (
