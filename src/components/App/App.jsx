07987fcf28a12081768f56f81add97b2518ebd0a
Carl Wilcoxon <62306383+CarlWilcoxon@users.noreply.github.com>
2021-06-05 21:38:56 -0500

    Refactor to support multiple webapps on one site

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,81 +1,33 @@
-import './App.css';
-import { useState, useEffect } from 'react';
-import ResultList from '../ResultList/ResultList';
-import { Button, Grid, TextField } from '@material-ui/core';
-const axios = require('axios');
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
-function App(props) {
-  const [operand1, setOperand1] = useState(0);
-  const [operand2, setOperand2] = useState(0);
-  const [resultArray, setResultArray] = useState([]);
-  const [history, setHistory] = useState([]);
+import Home from "../Home/Home";
+import CalcApp from "../CalcPage/CalcApp/CalcApp";
 
-  //handle sending the new result to the server
-  const handleSubmit = (evt) => {
-    evt.preventDefault();
-    const newResult = Number(operand1) + Number(operand2);
-    setResultArray(resultArray => [newResult, ...resultArray]);
-
-    axios.post('http://localhost:5000/add-history', newResult)
-      .then((res) => {
-        console.log(res);
-      })
-      .catch(error => console.error("Error adding new history."));
-  }
-
-  // const changeOperator
-  useEffect(() => {
-    getHistory();
-  }, [])
-
-  const getHistory = () => {
-    axios.get('http://localhost:5000/load-history')
-      .then((res) => {
-        const pastResults = res.data.pastResults;
-        setHistory(pastResults);
-      })
-      .catch(error => console.error("Error getting history."));
-  }
-
+function App() {
   return (
-    <div className="App">
-      <Grid
-        container
-        direction="column"
-        justify="center"
-        alignItems="center"
-      >
-
-        <Grid item>
-          <form onSubmit={handleSubmit} noValidate autoComplete="off">
-            <TextField
-              id="outlined-basic"
-              variant="outlined"
-              className="operand"
-              type="number"
-              value={operand1}
-              onChange={e => setOperand1(e.target.value)}
-              />
-
-            <TextField
-              id="outlined-basic"
-              variant="outlined"
-              className="operand"
-              type="number"
-              value={operand2}
-              onChange={e => setOperand2(e.target.value)}
-              />
-            <Button onClick={handleSubmit} variant="contained" color="primary">{"="}</Button>
-          </form>
-        </Grid>
-
-        <Grid item>
-          <ResultList resultArray={resultArray} />
-        </Grid>
-      </Grid>
-    </div>
+    <Router>
+      <div>
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/calc">
+            <CalcApp />
+          </Route>
+          {/* <Route path="/life">
+            <LifePage />
+          </Route> */}
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
 export default App;
-
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Link
+} from "react-router-dom";
+
+function Home() {
+  return (
+    <Router>
+        <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/calc">Calculator</Link>
+          </li>
+          <li>
+            <Link to="/life">Life</Link>
+          </li>
+        </ul>
+      </nav>
+    </Router>
+  );
+}
+
+export default Home;
